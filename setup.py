54213d7c084ad6e9d0520fb4aca9d99f008cccfb
Castleberry <cbcastleberry@summithealthcare.com>
2018-04-18 14:05:13 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='data, scripts and notebooks relating to the talkdata challenge on kaggle.',
+    author='Caleb Castleberry',
+    license='MIT',
+)
